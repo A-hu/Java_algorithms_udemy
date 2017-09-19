6c4ef9f3867e6836e8184157225a37629a71c2db
A-hu <louis0911317399@gmail.com>
2017-09-19 14:59:19 +0800

    Java basic

---
+++ basic/Earth.java
@@ -0,0 +1,20 @@
+public class Earth{
+    public static void main(String argsp[]){
+       //  Human tom;
+       //  tom = new Human();
+
+       //  tom.age = 5;
+       //  tom.eyeColor = "red";
+       //  tom.heightInInches = 100;
+       //  tom.name = "Tom Jelly";
+
+       //  tom.speak();
+       Human human1 = new Human("Wei", 123, 34, "black");
+       Human human2 = new Human("Weii", 1234, 345, "black and blue");
+       Human human3 = new Human("Weiii", 12345, 3456, "black and gold");
+
+       human1.speak();
+       human2.speak();
+       human3.speak();
+    }
+}
