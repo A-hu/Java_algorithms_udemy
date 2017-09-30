626cd5a2d903e8e6fa8d7ba450237f5f8bac66d0
A-hu <louis0911317399@gmail.com>
2017-09-30 17:52:15 +0800

    Singly Linked List

---
+++ learning-datastructures/src/ds/singlylinkedlist/App.java
@@ -0,0 +1,18 @@
+package ds.singlylinkedlist;
+
+public class App {
+
+	public static void main(String[] args) {
+		SinglyLinkedList myList = new SinglyLinkedList();
+		myList.insertFirst(100);
+		myList.insertFirst(200);
+		myList.insertFirst(300);
+		myList.insertFirst(400);
+		myList.insertFirst(500);
+		myList.insertLast(999);
+		
+		myList.displayList();
+
+	}
+
+}
